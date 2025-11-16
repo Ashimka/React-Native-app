50b625dfda2dc2c0eed1fb31b369acfd0981fdcd
Ashimka <ashimka2010@yandex.ru>
2025-11-16 19:52:21 +0500

    init: app react-native + tailwindcss

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-dark-200 font-bold">
+        Стартовый шаблон приложения с Tailwind CSS и Nativewind.
+      </Text>
+    </View>
+  );
+}
